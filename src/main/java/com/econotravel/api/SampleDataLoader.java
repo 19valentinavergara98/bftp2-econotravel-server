db895a9b62d5006baba2df3756e62d690669d02e
Laura Contreras <m.laura.cp89@gmail.com>
2022-02-04 12:41:39 +0100

    pequeños cambios sample data loader

--- src/main/java/com/econotravel/api/SampleDataLoader.java
+++ src/main/java/com/econotravel/api/SampleDataLoader.java
@@ -18,11 +18,11 @@ public class SampleDataLoader {
     @PostConstruct
     public void loadSampleData() {
         experienceRepository.saveAll(List.of(
-                new Experience("Paseo en bicicleta por el Montseny", "250", "5h", "montaña, bicicleta, excursión larga"),
-                new Experience("Descubre la costa en barco de vela", "280", "4h", "playa, barco, excursion larga"),
-                new Experience("Descubre la Barcelona Modernista de noche", "200", "4h", "ciudad, pie,excursion larga"),
-                new Experience("Del huerto a la mesa", "145", "3h", "montaña, a pie, excursion corta" ),
-                new Experience("Arte en la montaña sagrada", "2h", "125", "montaña, a pie, excursion corta")
+                new Experience("Paseo en bicicleta por el Montseny", 250, "5h", "montaña, bicicleta, excursión larga"),
+                new Experience("Descubre la costa en barco de vela", 280, "4h", "playa, barco, excursion larga"),
+                new Experience("Descubre la Barcelona Modernista de noche", 200, "4h", "ciudad, pie,excursion larga"),
+                new Experience("Del huerto a la mesa", 145 , "3h", "montaña, a pie, excursion corta" ),
+                new Experience("Arte en la montaña sagrada", 125, "2h", "montaña, a pie, excursion corta")
         ));
     }
 }
