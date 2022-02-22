b9455e398ccfc1345654e8fa3eeda3e0f67417c6
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-22 15:11:56 +0100

    implementado metodo delete

--- src/main/java/com/econotravel/api/ExperienceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.econotravel.api;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Experience not found")
-public class ExperienceNotFoundException extends RuntimeException {
-
-}
